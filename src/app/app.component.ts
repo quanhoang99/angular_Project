bca05d7bfeb881407a7d193b50ed223c4cc14fb1
quan.nguyenhoang2 <quan.nguyenhoang2@ivnd.com.vn>
2025-02-25 17:51:08 +0700

    444

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
 import {HeaderComponent} from "./component/header.component";
+import {NgStyle} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HeaderComponent],
+  imports: [HeaderComponent, NgStyle],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular_Project';
+  myStyle = {
+    'background-image': 'linear-gradient(to right, #d434b0, #8029fc)',
+    // 'height': 'calc(100vh - 70px)',
+  }
 }
--- src/app/component/test.component.ts
+++ src/app/component/test.component.ts
@@ -1,14 +1,20 @@
 import {Component} from "@angular/core";
+import {FormsModule} from "@angular/forms";
 
 @Component({
-  selector:'test-app',
-  standalone:true,
-  templateUrl:'./test.component.html',
+  selector: 'test-app',
+  standalone: true,
+  templateUrl: './test.component.html',
+  imports: [
+    FormsModule
+  ]
 })
-export class TestComponent{
-  nameBtn='Click me!';
-  clickMessage='';
-  onClick():void{
-    this.clickMessage='Hello World!';
+export class TestComponent {
+  nameBtn = 'Click me!';
+  clickMessage = '';
+  bindingMessage: string = '';
+
+  onClick(): void {
+    this.clickMessage = 'Hello World!';
   }
 }
